c15de1fdf87f737596c67d08a3c033e611e8e938
KevinChen435 <kevinchen435@gmail.com>
2020-07-09 11:33:04 -0400

    MVP

--- calc.py
+++ calc.py
@@ -1,1 +1,21 @@
-#hello
+def calc():
+    operators = ["+","-","*","/","exit"]
+    print('Welcome to the Calculator')
+    operator = ""
+    while operator not in operators:
+        operator = input("What operation?\n")
+        if operator == "exit":
+            exit()
+    number1 = int(input("First Number?\n"))
+    number2 = int(input("Second Number?\n"))
+    if operator == "+":
+        print(str(number1 + number2))
+    elif operator == "-":
+        print(str(number1 - number2))
+    elif operator == "*":
+        print(str(number1 * number2))
+    elif operator == "/":
+        print(str(number1 / number2))
+    else:
+        exit()
+calc()
